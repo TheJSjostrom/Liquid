57b0cf72bcf20baa63e95736600628eb3d1ffcba
TheJSjostrom <J.Sjostrom89@gmail.com>
2021-11-19 21:11:24 +0100

    Fixed a tipo.

--- Liquid/src/Liquid/Log.h
+++ Liquid/src/Liquid/Log.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <memory>
-#include "Core.h"
+#include "core.h"
 #include "spdlog/spdlog.h"
 
 namespace Liquid {
@@ -12,13 +12,13 @@ namespace Liquid {
 
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
 		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
 	};
 
 }
-
 
 // Core log macros
 #define LQ_CORE_TRACE(...)  ::Liquid::Log::GetCoreLogger()->trace(__VA_ARGS__)
