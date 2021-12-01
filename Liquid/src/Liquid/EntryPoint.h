44c2070a1547ecf16d8f84370044e375079b28ad
TheJSjostrom <J.Sjostrom89@gmail.com>
2021-12-01 21:14:53 +0100

    Fixes

--- Liquid/src/Liquid/EntryPoint.h
+++ Liquid/src/Liquid/EntryPoint.h
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
 	auto app = Liquid::CreateApplication();
 	app->Run();
 	delete app;
-
+	 
 }
 
 #endif
---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "Liquid"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Disk"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Liquid"
+	location "Liquid"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LQ_PLATFORM_WINDOWS",
+			"LQ_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox") 
+		}
+
+	filter "configurations:Debug"
+		defines "LQ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "LQ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "LQ_Dist"
+		symbols "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Liquid/vendor/spdlog/include",
+		"Liquid/src"
+	}
+
+	links
+	{
+		"Liquid"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LQ_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LQ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "LQ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "LQ_DIST"
+		symbols "On"
+
+ 
