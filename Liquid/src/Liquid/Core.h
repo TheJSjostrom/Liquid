3644cc55d554f3102ee293c54c9e48bd64415d35
TheJSjostrom <J.Sjostrom89@gmail.com>
2023-12-04 17:32:30 +0100

    We now linking runtime library dynamically; defined LQ_ENABLE_ASSERTS if LQ_DEBUG is on.

--- Liquid/src/Liquid/Core.h
+++ Liquid/src/Liquid/Core.h
@@ -10,6 +10,10 @@
 	#error Liquid Engine only supports Windows!
 #endif
 
+#ifdef LQ_DEBUG
+	#define LQ_ENABLE_ASSERTS
+#endif
+
 #ifdef LQ_ENABLE_ASSERTS
 	#define LQ_ASSERT(x, ...) { if(!(x)) { LQ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 	#define LQ_CORE_ASSERT(x, ...) { if(!(x)) { LQ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
